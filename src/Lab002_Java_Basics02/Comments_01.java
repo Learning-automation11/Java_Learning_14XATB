6b96db4ef81abf34358cbc31830971a3f74f3ea2
Learning-automation11 <shiva2.java@gmail.com>
2025-12-12 17:50:22 +0530

    This Java Basics_02 Practice Related

---
+++ src/Lab002_Java_Basics02/Comments_01.java
@@ -0,0 +1,12 @@
+package Lab002_Java_Basics02;
+
+public class Comments_01 {
+    static void main(String[] args) {
+        System.out.println("The One");
+        System.out.println("The One");
+        System.out.println("The One");
+        System.out.println("The One");
+        System.out.println("The One");
+
+    }
+}
