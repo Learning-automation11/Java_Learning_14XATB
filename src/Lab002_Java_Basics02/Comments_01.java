630baf224d894abdaac7b165fcbbc3328e35eecc
Learning-automation11 <shiva2.java@gmail.com>
2025-12-12 18:00:46 +0530

    comments added newly

--- src/Lab002_Java_Basics02/Comments_01.java
+++ src/Lab002_Java_Basics02/Comments_01.java
@@ -2,11 +2,13 @@ package Lab002_Java_Basics02;
 
 public class Comments_01 {
     static void main(String[] args) {
+/*
         System.out.println("The One");
         System.out.println("The One");
         System.out.println("The One");
         System.out.println("The One");
         System.out.println("The One");
+*/
 
     }
 }
