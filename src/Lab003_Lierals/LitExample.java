f1f18ce5cfd5f9fafc3456c1400c38821809f6ef
Learning-automation11 <shiva2.java@gmail.com>
2026-01-09 07:16:38 +0530

    with package

---
+++ src/Lab003_Lierals/LitExample.java
@@ -0,0 +1,10 @@
+package Lab003_Lierals;
+
+public class LitExample {
+   public static void main() {
+
+       int i=10;
+       // D(Datatype) I(Integral) L(Literal)
+       System.out.println("Integer value "+i);
+    }
+}
