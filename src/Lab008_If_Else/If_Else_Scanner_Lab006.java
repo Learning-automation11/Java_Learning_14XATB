f9a891dd5b8f49b61cacaba0224be3eec95603f2
Learning-automation11 <shiva2.java@gmail.com>
2026-02-04 18:27:31 +0530

    program from my desktop

---
+++ src/Lab008_If_Else/If_Else_Scanner_Lab006.java
@@ -0,0 +1,15 @@
+package Lab008_If_Else;
+
+public class If_Else_Scanner_Lab006 {
+    static void main(String[] args) {
+        // jdk > 13
+        int itemcode = 001;
+        switch (itemcode){
+            case 001 -> System.out.println("001");
+            case 002 -> System.out.println("002");
+            case 003 -> System.out.println("003");
+            default -> System.out.println("default");
+
+        }
+    }
+}
---
+++ src/Lab008_If_Else/If_Else_switch_abc_Scanner_Lab007.java
@@ -0,0 +1,24 @@
+package Lab008_If_Else;
+
+public class If_Else_switch_abc_Scanner_Lab007 {
+    static void main(String[] args) {
+        char letter= 'b';
+        switch(letter){
+
+            case 'a' :
+                System.out.println("The letter is a");
+                break;
+            case 'b' :
+                System.out.println("The letter is b");
+                break;
+            case 'c' :
+                System.out.println("The letter is c");
+                break;
+            default :
+                System.out.println("Default");
+                break;
+
+        }
+    }
+
+}
